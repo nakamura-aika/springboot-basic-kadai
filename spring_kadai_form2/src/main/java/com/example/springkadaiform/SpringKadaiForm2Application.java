35cd49bc7f73deb69e15dfb27a88ef4e2c03a9ca
nakamura-aika <a.nakamura.cms@gmail.com>
2025-08-20 13:36:30 +0900

    初回コミット

---
+++ spring_kadai_form2/src/main/java/com/example/springkadaiform/SpringKadaiForm2Application.java
@@ -0,0 +1,13 @@
+package com.example.springkadaiform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKadaiForm2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKadaiForm2Application.class, args);
+	}
+
+}
---
+++ spring_kadai_form2/src/test/java/com/example/springkadaiform/SpringKadaiForm2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkadaiform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKadaiForm2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
